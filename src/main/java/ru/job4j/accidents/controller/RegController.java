3ed8e4698712992376e53be68fac5cd93abe4ed0
Антон <lopatuxin.a@gmail.com>
2023-05-18 14:11:25 +0300

    2. Регистрация пользователя [#296069]

--- src/main/java/ru/job4j/accidents/controller/RegController.java
+++ src/main/java/ru/job4j/accidents/controller/RegController.java
@@ -3,9 +3,12 @@ package ru.job4j.accidents.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.AuthorityRepository;
 import ru.job4j.accidents.repository.UserRepository;
@@ -19,7 +22,11 @@ public class RegController {
     private final AuthorityRepository authorities;
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
+    public String regSave(@ModelAttribute User user, Model model) {
+        if (users.getByName(user.getUsername()) != null) {
+            model.addAttribute("message", "Пользователь с таким именем существует");
+            return "errors/404";
+        }
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
--- src/main/java/ru/job4j/accidents/repository/UserRepository.java
+++ src/main/java/ru/job4j/accidents/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package ru.job4j.accidents.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.accidents.model.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+    @Query("select u from User u where username = :name")
+    User getByName(@Param("name") String username);
 }
