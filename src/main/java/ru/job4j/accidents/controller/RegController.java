e62edfacd96528304b450a9372c3caa474a6d734
Антон <lopatuxin.a@gmail.com>
2023-05-21 13:01:24 +0300

    2. Регистрация пользователя [#296069]

--- src/main/java/ru/job4j/accidents/controller/RegController.java
+++ src/main/java/ru/job4j/accidents/controller/RegController.java
@@ -1,14 +1,16 @@
 package ru.job4j.accidents.controller;
 
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.AuthorityRepository;
 import ru.job4j.accidents.repository.UserRepository;
@@ -21,21 +23,30 @@ public class RegController {
     private final UserRepository users;
     private final AuthorityRepository authorities;
 
+    private final Logger LOG = LogManager.getLogger(RegController.class);
+
     @PostMapping("/reg")
     public String regSave(@ModelAttribute User user, Model model) {
-        if (users.getByName(user.getUsername()) != null) {
-            model.addAttribute("message", "Пользователь с таким именем существует");
-            return "errors/404";
-        }
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        users.save(user);
+        try {
+            users.save(user);
+        } catch (DataIntegrityViolationException e) {
+            model.addAttribute("message", "Такой пользователь уже существует");
+            return "errors/404";
+        }
         return "redirect:/login";
     }
 
     @GetMapping("/reg")
     public String regPage() {
         return "reg";
+    }
+
+    @ExceptionHandler(value = {DataIntegrityViolationException.class})
+    public String getException(Exception e) {
+        LOG.error(e.getLocalizedMessage(), e);
+        return "redirect:/reg?error = true";
     }
 }
